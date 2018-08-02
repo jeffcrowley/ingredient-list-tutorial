88dd263f4f3c01c5aa2f8f17bff80083432b0a2b
jeffcrowley <jeffcrowley@Jeffs-MacBook-Pro.local>
2018-08-02 16:14:16 -0700

    seed drinks, render JSON in controllers

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module IngredientList
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.app_generators.scaffold_controller = :scaffold_controller
 
     #middleware for activeadmin stuff -jc
     config.middleware.use Rack::MethodOverride
